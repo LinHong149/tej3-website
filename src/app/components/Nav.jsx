8d089c664506f1201a827614cf8128bd2aef740e
LinHong <linlin.hong1@gmail.com>
2024-04-28 19:54:28 -0400

    Added current page to nav

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -3,6 +3,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 import React from 'react'
+import { useRouter } from "next/router"
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from "../../../public/logo.png"
@@ -10,6 +11,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
+  const router = useRouter()
+
+  const isActive = (path) => {
+    return router.pathname === path ? 'opacity-100' : 'opacity-50';
+  };
+
   return (
     <div className='absolute top-0 left-0 z-100 w-full flex justify-between px-[120px] py-[2.5rem] absolute top-0 left-0 z-100 items-center'>
         <Link href="/">
@@ -18,11 +25,11 @@ const Nav = () => {
             <h2 className='title text-3xl text-highlight'>Macaron Bakery</h2>
           </div>
         </Link>
-        <div className='nav flex gap-6 [&>*]:text-2xl [&>*]:opacity-50 hover:[&>*]:opacity-100'>
-          <Link href="/" className='title'>Home</Link>
-          <Link href="/products" className='title'>Products</Link>
-          <Link href="/about" className='title'>About</Link>
-          <Link href="/contact" className='title'>Contact</Link>
+        <div className='nav flex gap-6 [&>*]:text-2xl'>
+          <Link href="/" className={isActive('/') + ' title'}>Home</Link>
+          <Link href="/products" className={isActive('/products') + ' title'}>Products</Link>
+          <Link href="/about" className={isActive('/about') + ' title'}>About</Link>
+          <Link href="/contact" className={isActive('/contact') + ' title'}>Contact</Link>
         </div>
         <div className='rounded-full bg-secondary w-12 h-12 flex items-center pt-1 justify-center'>
           <Link href="/cart"><FontAwesomeIcon className='w-5 h-5' size='2x' color='white' icon={faShoppingCart} /></Link>
